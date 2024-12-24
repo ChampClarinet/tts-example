3fb37818619cb778f24dc55dd611f2d10df11860
Wallop Opasakhun (Windows) <champbassist159@gmail.com>
2024-12-24 13:30:49 +0700

    tts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,54 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .decorate("getAPIKey", () => {
+    const apiKey = process.env.API_KEY;
+    if (!apiKey) throw new Error("API_KEY is not defined");
+    return apiKey;
+  })
+  .post(
+    "/tts",
+    async ({ body, getAPIKey, set }) => {
+      if (body.text.length == 0) {
+        set.status = 204;
+        return;
+      }
+      const payload = {
+        text: body.text,
+        speaker: "1",
+        volume: 1,
+        speed: 1,
+        type_media: "m4a",
+        save_file: true,
+      };
+      const response = await fetch(
+        "https://api-voice.botnoi.ai/openapi/v1/generate_audio",
+        {
+          headers: {
+            "Botnoi-Token": getAPIKey(),
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+          method: "POST",
+        }
+      );
+
+      const jsonResponse: { text: string; audio_url: string; point: number } =
+        await response.json();
+      const link = jsonResponse.audio_url;
+      const audioResponse = await fetch(link);
+      const audioBlob = await audioResponse.blob();
+      return audioBlob;
+    },
+    {
+      body: t.Object({
+        text: t.String(),
+      }),
+    }
+  )
+  .post("/stt", ({ body, set }) => {}, { body: t.File() })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
